b15fb6cbdd9a8fad5ccf767dad57ef223e3e2a0c
Lukas Gonzalez <lukegonz47@Lukass-MBP.attlocal.net>
2022-12-31 20:47:22 -0500

    added signup component with working link

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import Home from './components/Home'
+import Signup from './components/Signup';
+import { Link, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="body">
       <Home />
+      <Routes>
+        <Route path='/signup' element={<Signup />}/>
+      </Routes>
     </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,12 @@
-
+import { TextField } from "@mui/material";
+import { Link } from 'react-router-dom';
 
 function Home() {
     return (
         <div className="home-div">
-            <p>SEARCHBAR</p>
-            <h1>Welcome to MineChat!</h1>
-            <h3>SignUp</h3> {/**this will appear as 'profile' when logged in */}
+            <TextField className="searchbar" id="standard-basic" label="Search" variant="standard" />
+            <h1 className="title">Welcome to MineChat!</h1>
+            <Link to='/signup' className="signup"><h3>SignUp</h3></Link> {/**this will appear as 'profile' when logged in */}
         </div>
     )
 }
