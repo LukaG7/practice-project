6d5fcde44e13e1fef1916d1520679fc5c4c29f04
Lukas Gonzalez <lukegonz47@Lukass-MBP.attlocal.net>
2022-12-31 20:11:00 -0500

    cleared css and added some componenets in Homejs

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,10 @@
 
 function Home() {
     return (
-        <div>
+        <div className="home-div">
+            <p>SEARCHBAR</p>
             <h1>Welcome to MineChat!</h1>
+            <h3>SignUp</h3> {/**this will appear as 'profile' when logged in */}
         </div>
     )
 }
