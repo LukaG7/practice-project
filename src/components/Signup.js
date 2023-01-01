392c692c4056f32bd016ba79f87f792a6f4a1e6a
Lukas Gonzalez <lukegonz47@gmail.com>
2023-01-01 18:20:37 -0500

    added Home link into Signup component

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,8 +1,10 @@
-
+import Home from "./Home";
+import { Link } from "react-router-dom";
 
 function Signup() {
     return (
         <div>
+            <Link to='/'><h3>Home</h3></Link>
             <h1>Signup page is working and loaded :D</h1>
         </div>
     )
