55e515f8365053e59efeeb2f2e62cb48181ebc5d
Lukas Gonzalez <lukegonz47@Lukass-MacBook-Pro.local>
2023-01-02 16:15:48 -0500

    test commit

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -118,7 +118,7 @@ export default function SignUp() {
             <Grid container justifyContent="flex-end">
               <Grid item>
                 <Link href="#" variant="body2">
-                  Already have an account? Sign in
+                  Already have an account? Sign in!
                 </Link>
               </Grid>
             </Grid>
