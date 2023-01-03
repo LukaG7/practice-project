3dc6088ec20f6bf723f6a7bff5e70c4a9588e5f4
Lukas Gonzalez <lukegonz47@Lukass-MacBook-Pro.local>
2023-01-02 19:10:50 -0500

    added new background

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ function Home() {
         <div className="home-div">
             <TextField className="searchbar" id="standard-basic" label="Search" variant="standard" />
             <h1 className="title">Welcome to MineChat!</h1>
-            <Link to='/signup' className="signup"><h3>SignUp</h3></Link> {/**this will appear as 'profile' when logged in */}
+            <Link to='/signin' className="signup"><h3>Sign In</h3></Link> {/**this will appear as 'profile' when logged in */}
             <Link to='/' className="homelink"><h3>Home</h3></Link>
         </div>
     )
