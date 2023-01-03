e49fe28031240b8fc88c51accdc70d6d23e82ac5
Lukas Gonzalez <lukegonz47@Lukass-MacBook-Pro.local>
2023-01-02 19:00:22 -0500

    added home link, and signincomponent

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Home from './components/Home'
 import SignUp from './components/SignUp';
+import SignIn from './components/SignIn';
 import { Route, Routes } from 'react-router-dom';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Home />
       <Routes>
         <Route path='/signup' element={<SignUp />}/>
+        <Route path='/signin' element={<SignIn />}/>
       </Routes>
     </div>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,14 @@
 import { TextField } from "@mui/material";
 import { Link } from 'react-router-dom';
 
+
 function Home() {
     return (
         <div className="home-div">
             <TextField className="searchbar" id="standard-basic" label="Search" variant="standard" />
             <h1 className="title">Welcome to MineChat!</h1>
             <Link to='/signup' className="signup"><h3>SignUp</h3></Link> {/**this will appear as 'profile' when logged in */}
+            <Link to='/' className="homelink"><h3>Home</h3></Link>
         </div>
     )
 }
