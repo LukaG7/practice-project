164c38f8a30246fc287fa11d0edc310097e2d490
Lukas Gonzalez <lukegonz47@gmail.com>
2023-01-03 00:02:45 -0500

    updated spelling error in appjs

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Home from './components/Home'
-import SignUp from './components/SignUp';
+import Signup from './components/Signup';
 import SignIn from './components/SignIn';
 import { Route, Routes } from 'react-router-dom';
 
@@ -9,7 +9,7 @@ function App() {
     <div className="body">
       <Home />
       <Routes>
-        <Route path='/signup' element={<SignUp />}/>
+        <Route path='/signup' element={<Signup />}/>
         <Route path='/signin' element={<SignIn />}/>
       </Routes>
     </div>
