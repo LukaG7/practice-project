1a1d6881bb14c263cf63f6e6b704ba91a510b289
Lukas Gonzalez <lukegonz47@gmail.com>
2023-01-17 16:21:18 -0500

    added seperate Home.js component with basic setup

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import NavBar from './components/NavBar';
 import Signup from './components/Signup';
 import SignIn from './components/SignIn';
+import Home from './components/Home';
 import { Route, Routes } from 'react-router-dom';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path='/signup' element={<Signup />}/>
         <Route path='/signin' element={<SignIn />}/>
+        <Route path='/' element={<Home />}/>
       </Routes>
     </div>
   );
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,7 +6,6 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
-import SignIn from './SignIn';
 import { Link } from 'react-router-dom';
 
 export default function NavBar() {
